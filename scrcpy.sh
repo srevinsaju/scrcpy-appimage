6ec0b61c9a30f48084237a9a359c6090dcc3a441
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-26 16:06:00 +0300

    :tada: Initial commit

---
+++ scrcpy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+rm -rf AppDir
+mkdir AppDir
+export APPIMAGE_EXTRACT_AND_RUN=1
+./linuxdeploy*.AppImage -e /usr/local/bin/scrcpy --desktop-file $SRC_ROOT/scrcpy.desktop --icon-file $SRC_ROOT/scrcpy.png --appdir=AppDir --library=/usr/lib/libc.so.6 --library=/usr/lib64/ld-linux-x86-64.so.2 --custom-apprun=$SRC_ROOT/AppRun
+
+cp $BUILD_DIR/app/scrcpy AppDir/usr/bin/.
+cp $ADB_DIR/adb AppDir/usr/bin/.
+
+mkdir -p AppDir/usr/local/share/scrcpy
+cp $BUILD_DIR/../scrcpy_server AppDir/usr/local/share/scrcpy/scrcpy-server
+
+./appimage*.AppImage AppDir -s AppDir/usr/share/applications/scrcpy.desktop -l -o
