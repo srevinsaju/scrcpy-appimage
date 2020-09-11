188ab8d7db6f269e75192f35e39e33eab436c31b
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-11 16:20:04 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -eu
 rm -rf AppDir
 mkdir AppDir
 export APPIMAGE_EXTRACT_AND_RUN=1
-./linuxdeploy*.AppImage -e /usr/local/bin/scrcpy --desktop-file $SRC_ROOT/scrcpy.desktop --icon-file $SRC_ROOT/scrcpy.png --appdir=AppDir --library=/usr/lib/libc.so.6 --library=/usr/lib64/ld-linux-x86-64.so.2 --custom-apprun=$SRC_ROOT/AppRun -lavformat
+./linuxdeploy*.AppImage -e /usr/local/bin/scrcpy --desktop-file $SRC_ROOT/scrcpy.desktop --icon-file $SRC_ROOT/scrcpy.png --appdir=AppDir --library=/usr/lib/libc.so.6 --library=/usr/lib64/ld-linux-x86-64.so.2 --custom-apprun=$SRC_ROOT/AppRun --library=/usr/lib/libavformat.so.58
 
 cp $BUILD_DIR/app/scrcpy AppDir/usr/bin/.
 cp $ADB_DIR/adb AppDir/usr/bin/.
